1de9366c3444a73ad3ee72653417ed04af38850e
Andrei Dziahel <develop7@develop7.info>
2018-10-29 22:24:08 +0300

    move domain existence check to the controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :find_domain
   def index
     if params[:search]
       @posts = Post.includes(:authors).search(params[:search]) # change this to search after implementing it
@@ -9,5 +10,15 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find_by(domain: params[:domain], slug: params[:slug])
+  end
+
+  private
+
+  def find_domain
+    domain = params[:domain]
+    unless Post.where(domain: domain).count.positive?
+      flash[:error] = "Couldn't open blog #{domain}"
+      redirect_to('/blog')
+    end
   end
 end
